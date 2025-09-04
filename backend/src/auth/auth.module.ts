0a59fc0fd52189871f4136068d7e809d38168049
Hozaifa <hozaifabinmasud@gmail.com>
2025-09-04 19:44:47 +0600

    Fixed import validation

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 import { AuthService } from '@/auth/auth.service';
 import { AuthController } from '@/auth/auth.controller';
 import { UserModule } from '@/user/user.module';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { LocalStrategy } from './strategies/local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
+import jwtConfig from '@/config/jwt.config';
 
 @Module({
-   providers: [AuthService],
+   imports: [
+      JwtModule.registerAsync(jwtConfig.asProvider()),
+      ConfigModule.forFeature(jwtConfig),
+      UserModule,
+   ],
+   providers: [AuthService, JwtStrategy, LocalStrategy],
    controllers: [AuthController],
-   imports: [UserModule],
 })
 export class AuthModule {}
